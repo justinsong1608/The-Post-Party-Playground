9f5fe1cc84873f5adbb0c324d2da9f84078039dd
Justin Song <justinsong1608@gmail.com>
2023-05-10 22:40:23 +0000

    Completed modal.

--- client/src/components/NewsLetter.jsx
+++ client/src/components/NewsLetter.jsx
@@ -2,11 +2,12 @@ import { FaMailchimp } from 'react-icons/fa';
 import './componentsCSS/NewsLetter.css';
 
 export default function NewsLetter() {
+
   return (
     <form className="d-flex">
       <div className="input-group center">
         <input className="form-control-md input" type="email" placeholder="Email" aria-label="Email" size={20} />
-        <button className="btn btn-primary" type="submit"><FaMailchimp size={25} /></button>
+        <button className="btn btn-primary" type="button"><FaMailchimp size={25} /></button>
       </div>
     </form>
   );
